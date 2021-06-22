4a8ba6557359d504968f9b9c2fd6ada6c0c8f4b8
FedericoACaivano <49082078+FedericoACaivano@users.noreply.github.com>
2021-06-22 11:46:16 -0300

    Add files via upload
    Se requiere procesar una lista de personas físicas y jurídicas para generar los CUIL y CUIT. Un CUIL es un prefijo de dos dígitos, seguido de un guión, seguido del DNI de 8 dígitos, seguido de un sufijo de un dígito. 
    El prefijo y el sufijo se calculan según las siguientes reglas de formación: 
    1. Se multiplica cada uno de los 8 dígitos del DNI, comenzando por el más significativo, por un factor del conjunto {3,2,7,6,5,4,3,2}, en ese orden y se suman los resultados de esos productos. 
    2. A la suma anterior se le suma 10 si es varón o 38 si es mujer. 
    3. Se calcula el resto de dividir esa suma por 11. 
    4. Si el resto es uno, entonces el prefijo es 23, y el sufijo es 9 para varón y 4 para mujer. 
    5. Si el resto es cero, entonces el prefijo es 20 para varón y 27 para mujer, y el sufijo es cero. 
    6. En cualquier otro caso el prefijo es 20 para varón y 27 para mujer, y el sufijo es 11 menos el resto. 
    Por ejemplo, dado un varón con DNI 08424054, (0*3+8*2+4*7+2*6+4*5+0*4+5*3+4*2=99 -> (99+10)%11=10 
    El resto es 10 Sufijo=11-10 = 1, entonces, sufijo1,como sexo es varón prefijo 20, por tanto su CUIL es 20-08424054-1.

---
+++ Ej. 2.5.cpp
@@ -0,0 +1,102 @@
+#include <iostream>
+using namespace std;
+
+//int CalcularPrefijo(int, string);
+//int CalcularSufijo(int, string);
+
+
+int main(int argc, char** argv){
+	int DNI = 0;
+	string sexo = "";
+	
+	int prefijo;
+	int sufijo;
+	
+/*------CHEQUEO DE INGRESOS VÁLIDOS-----------*/
+	cout << "Ingrese el numero de DNI: " << endl;
+	cin >> DNI;	
+	
+	while ((DNI < 0) || (DNI > 100000000)){
+	cout << "Ingreso incorrecto. Ingrese el DNI nuevamente: " << endl;
+	cin >> DNI;	
+	}
+	
+		cout << "Ingrese el sexo (hombre o mujer): " << endl;
+		cin >> sexo;
+		
+	while ((sexo != "hombre") && (sexo != "mujer")){
+		cout << "Ingreso incorrecto. Ingrese el sexo nuevamente: " << endl;
+		cin >> sexo;
+	}
+/*----------FIN CHEQUEO-------------*/
+
+//separo el DNI en cada una de sus cifras
+//saco la primera cifra como cociente ("corriendo la coma") de dividir el dni por 10000000
+//repito con las demás, ajustando con los valores anteriores.
+int pri = DNI / 10000000;
+int seg = (DNI / 1000000) - pri*10;
+int ter = (DNI / 100000) - (DNI / 1000000)*10;	
+int cua = (DNI / 10000) - (DNI / 100000)*10;
+int qui = (DNI / 1000) - (DNI / 10000)*10;
+int sex = (DNI / 100) - (DNI / 1000)*10;
+int sep = (DNI / 10) - (DNI / 100)*10;
+int oct = (DNI / 1) - (DNI / 10)*10;
+	
+
+/*-----Paso 1-----*/
+int suma = ((pri*3)+(seg*2)+(ter*7)+(cua*6)+(qui*5)+(sex*4)+(sep*3)+(oct*2));
+
+
+/*-----Paso 2-----*/
+if(sexo == "hombre"){
+	suma += 10;
+}
+if(sexo == "mujer"){
+	suma += 38;
+}
+
+/*-----Paso 3-----*/
+int resto = suma % 11;
+
+/*-----Paso 4-----*/
+if (resto == 1){
+	if  (sexo == "hombre"){
+	prefijo = 23;
+	sufijo = 9;	
+	}
+	if  (sexo == "mujer"){
+		prefijo = 23;
+		sufijo = 4;
+	}
+}
+
+/*-----Paso 5-----*/
+if (resto == 0){
+	if  (sexo == "hombre"){
+	prefijo = 20;
+	sufijo = 0;	
+	}
+	if  (sexo == "mujer"){
+		prefijo = 27;
+		sufijo = 0;
+	}
+}
+
+/*-----Paso 6-----*/
+if ((resto != 0) && (resto != 1)){
+	if  (sexo == "hombre"){
+	prefijo = 20;
+	sufijo = 11-resto;	
+	}
+	if  (sexo == "mujer"){
+		prefijo = 27;
+		sufijo = 11-resto;
+	}
+}
+
+cout << "El CUIL solicitado es: " << prefijo << "-" << DNI << "-" << sufijo << endl;
+
+return 0;
+}
+
+
